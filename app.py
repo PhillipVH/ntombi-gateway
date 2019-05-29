454bcad3d0e09eaefa410cec4504010ff074bcd8
Phillip van Heerden <vanheerden.phillip@gmail.com>
2019-05-29 22:35:06 +0200

    Individual event handlers

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import json
 
-from flask import Flask, request, abort
+from flask import Flask, request, abort, render_template
 from kafka import KafkaProducer
-
 
 # Default Flask configuration
 app = Flask(__name__)
@@ -11,11 +10,20 @@ app = Flask(__name__)
 producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    app.logger.info('Event: ' + str(request.get_json()))
-    process_event(request.get_json())
-    return ''
+    """
+    The root endpoint of the gateway system. Dispensers POST to this endpoint, signaling events like STARTUP,
+    DISPENSE, and REFILL_REQUEST. Browsers can GET this endpoint for a overview of all the registered dispensers.
+    POSTed events to this endpoint are ingested into Kafka, for use by other value adding applications.
+    """
+
+    if request.method == 'POST':
+        app.logger.info('Event: ' + str(request.get_json()))
+        process_event(request.get_json())
+        return ''
+    else:
+        return render_template('index.html')
 
 
 def permitted(id, token):
@@ -49,15 +57,52 @@ def process_event(event):
 
     # If any of the required parameters are absent, reject the request.
     if (not id) or (not token) or (not type):
+        app.logger.warn('Request rejected: Required field absent')
+        abort(400)
+
+    # Message type must be on the allowed types
+    legal_types = ['STARTUP', 'DISPENSE', 'BATTERY', 'REFILL']
+    if type not in legal_types:
+        app.logger.warn('Request rejected: Invalid event type')
         abort(400)
 
     # Authenticate the dispenser against our database of tokens.
     if not permitted(event['id'], event['token']):
+        app.logger.warn('Request rejected: Invalid authentication token')
         abort(401)
+
+    # Dispatch event to appropriate handler
+    if type == 'STARTUP':
+        handle_startup(event)
+    elif type == 'DISPENSE':
+        handle_dispense(event)
+    elif type == 'REFILL':
+        handle_refill_request(event)
+    elif type == 'REFILLED':
+        handle_refilled(event)
 
     # Send the event to the dispensers Kafka topic
     # TODO Topic for startup, dispense, battery level, refill request
-    producer.send('dispensers', event)
+    # producer.send(type, event)
+
+
+def handle_startup(event):
+    app.logger.info('Processing STARTUP event: ' + str(event))
+    return
+
+
+def handle_dispense(event):
+    app.logger.info('Processing DISPENSE event: ' + str(event))
+    return
+
+
+def handle_refill_request(event):
+    app.logger.info('Processing REFILL event: ' + str(event))
+    pass
+
+
+def handle_refilled(event):
+    app.logger.info('Processing REFILLED event: ' + str(event))
 
 
 if __name__ == '__main__':
